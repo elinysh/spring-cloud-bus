889542aecc7d113073cb058bc265d9d9834b0c63
user <36792>
2021-03-29 23:26:52 +0900

    spring cloud server config 설정

--- src/main/java/com/example/cloudconfig/CloudConfigApplication.java
+++ src/main/java/com/example/cloudconfig/CloudConfigApplication.java
@@ -1,10 +1,12 @@
 package com.example.cloudconfig;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @EnableConfigServer // spirng cloud server 설정
 public class CloudConfigApplication {
     public static void main(String[] args) {
